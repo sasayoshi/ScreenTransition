388af7b7fe55c29ea545658dc812b067595b053d
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-21 19:55:27 +0900

    Initial Commit

---
+++ ScreenTransition/ScreenTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTransitionApp.swift
+//  ScreenTransition
+//
+//  Created by sasayoshi on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
